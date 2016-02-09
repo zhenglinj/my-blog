b53f7e2b336813b52833ba835a43dcf2aad477ec
zhenglinj <zhenglinj@hotmail.com>
2016-02-09 23:29:24 +0800

    Update posts

--- _posts/code/xxx_sort.py
+++ _posts/code/xxx_sort.py
@@ -2,6 +2,28 @@
 
 import random
 
+# Selection sort
+def selection_sort(nums):
+    for i in range(0, len(nums)):
+        min_idx = i
+        for j in range(i, len(nums)):
+            if (nums[j] < nums[min_idx]):
+                min_idx = j
+        nums[i], nums[min_idx] = nums[min_idx], nums[i]
+    return
+
+# Bubble sort
+def bubble_sort(nums):
+    for i in range(len(nums), 1, -1):
+        for j in range(1, i):
+            if (nums[j-1] > nums[j]):
+                nums[j-1], nums[j] = nums[j], nums[j-1]
+    return
+
+# Heapsort
+def heap_sort(nums):
+    pass                        # TODO: too hard
+
 # Quick sort
 def quick_sort(nums):
     def _quick_sort(nums, l, r):
@@ -28,34 +50,6 @@ def quick_sort(nums):
             _quick_sort(nums, low+1, r)
             print("   "*l, "Concat: ", nums[l:r+1], sep="")
         return
-
-    # def _quick_sort(nums, l, r):
-    #     if (l < r):
-    #         key = nums[l]     # TODO
-    #         low, high = l, r
-    #         while (low < high):
-    #             while (low < high and
-    #                    nums[high] >= key):
-    #                 high -= 1
-    #             if (low < high):
-    #                 nums[low] = nums[high]
-    #                 low += 1
-    #             while (low < high and
-    #                    nums[low] <= key):
-    #                 low += 1
-    #             if (low < high):
-    #                 nums[high] = nums[low]
-    #                 high -= 1
-    #         nums[low] = key
-    #         print("   "*l, "Devide: ",
-    #               nums[l:low] if nums[l:low] else "",
-    #               " ", nums[low], " ",
-    #               nums[low+1:r+1] if nums[low+1:r+1] else "",
-    #               sep="")
-    #         _quick_sort(nums, l, low-1)
-    #         _quick_sort(nums, low+1, r)
-    #         print("   "*l, "Concat: ", nums[l:r+1], sep="")
-    #    return
 
     _quick_sort(nums, 0, len(nums)-1)
 
@@ -93,45 +87,12 @@ def merge_sort(nums, indent=""):
     return
 
 
-# Heapsort
-def heap_sort(nums):
-    pass                        # TODO: too hard
-
-
-# Selection sort
-def selection_sort(nums):
-    for i in range(0, len(nums)):
-        min_idx = i
-        for j in range(i, len(nums)):
-            if (nums[j] < nums[min_idx]):
-                min_idx = j
-        nums[i], nums[min_idx] = nums[min_idx], nums[i]
-    return
-
-# Bubble sort
-def bubble_sort(nums):
-    for i in range(len(nums), 1, -1):
-        for j in range(1, i):
-            if (nums[j-1] > nums[j]):
-                nums[j-1], nums[j] = nums[j], nums[j-1]
-    return
-
 if __name__ == '__main__':
     # nums0 = [2, 3, 5, 1, 4]
     # nums0 = [6, 2, 7, 3, 8, 9]
     # nums0 = [7, 7, 5, 8, 5, 2, 1]
     nums0 = list(range(0, 10))
     random.shuffle(nums0)
-
-    nums = nums0[:]
-    print("\nQuick sort\n", "Origin: ", nums, sep="")
-    quick_sort(nums)
-    print("Result: ", nums, sep="")
-
-    nums = nums0[:]
-    print("\nMerge sort\n", "Origin: ", nums, sep="")
-    merge_sort(nums)
-    print("Result: ", nums, sep="")
 
     nums = nums0[:]
     print("\nSelection sort\n", "Origin: ", nums, sep="")
@@ -141,4 +102,14 @@ if __name__ == '__main__':
     nums = nums0[:]
     print("\nBubble sort\n", "Origin: ", nums, sep="")
     bubble_sort(nums)
+    print("Result: ", nums, sep="")
+
+    nums = nums0[:]
+    print("\nQuick sort\n", "Origin: ", nums, sep="")
+    quick_sort(nums)
+    print("Result: ", nums, sep="")
+
+    nums = nums0[:]
+    print("\nMerge sort\n", "Origin: ", nums, sep="")
+    merge_sort(nums)
     print("Result: ", nums, sep="")
