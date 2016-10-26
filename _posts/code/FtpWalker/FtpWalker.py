c973359a5bd5f1af1c46577d53b88151048cd27d
zhenglinj <zhenglinj@hotmail.com>
2016-10-26 23:06:34 +0800

    Update code

--- _posts/code/FtpWalker/FtpWalker.py
+++ _posts/code/FtpWalker/FtpWalker.py
@@ -6,6 +6,7 @@ from ftptool import FTPHost
 
 # ftp://ftp.gnu.org/
 a_host = FTPHost.connect("ftp://ftp.gnu.org/", user="anonymous", password="", timeout=1000)
+
 a_host.current_directory = "/"
 
 for (dirname, subdirs, files) in a_host.walk("/"):
@@ -14,6 +15,15 @@ for (dirname, subdirs, files) in a_host.walk("/"):
             subdirs.remove(subdir)
     print(dirname, "has file(s)", ", ".join(files))
 
+# print tree
+def FtpWalker(dirname, prefix = ""):
+    a_host.current_directory = dirname
+    (subdirs, files) = a_host.listdir(dirname)
+    print(prefix, dirname, ": ", ", ".join(files), sep="")
+    for subdir in subdirs:
+        FtpWalker(dirname+"/"+subdir, prefix = prefix+"\t")
+
+FtpWalker("/")
 
 # Local Variables:
 # python-shell-interpreter: "python"
