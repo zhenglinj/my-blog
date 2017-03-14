a77d8a269f63bfacc60c1b5fd7280945cc8ae34f
zhenglinj <zhenglinj@hotmail.com>
2017-03-14 13:11:56 +0800

    Add app entry (react-app layout page) and add cloudtie

---
+++ reactjs/entry.jsx
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Tabs, Tab, Row, Col } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import Hello from './components/Hello.jsx';
+
+const tabsInstance = (
+  <Tab.Container id="tabs-with-dropdown" defaultActiveKey="1">
+    <Row className="clearfix">
+      <Col sm={12}>
+        <Nav bsStyle="tabs">
+          <NavItem eventKey="0" disabled>
+            Online Applications
+          </NavItem>
+          <NavItem eventKey="1">
+            Tab 1
+          </NavItem>
+          <NavItem eventKey="2">
+            Tab 2
+          </NavItem>
+          <NavDropdown eventKey="3" title="Dropdown" id="nav-dropdown-within-tab">
+            <MenuItem eventKey="3.1">Action</MenuItem>
+            <MenuItem eventKey="3.2">Another action</MenuItem>
+            <MenuItem eventKey="3.3">Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey="3.4">Separated link</MenuItem>
+          </NavDropdown>
+        </Nav>
+      </Col>
+      <Col sm={12}>
+        <Tab.Content animation>
+          <Tab.Pane eventKey="1">
+            {/* Tab 1 content */}
+            <p><Hello prompt="tab1" /></p>
+          </Tab.Pane>
+          <Tab.Pane eventKey="2">
+            {/* Tab 2 content */}
+          </Tab.Pane>
+          <Tab.Pane eventKey="3.1">
+            {/* Tab 3.1 content */}
+          </Tab.Pane>
+          <Tab.Pane eventKey="3.2">
+            {/* Tab 3.2 content */}
+          </Tab.Pane>
+          <Tab.Pane eventKey="3.3">
+            {/* Tab 3.3 content */}
+          </Tab.Pane>
+          <Tab.Pane eventKey="3.4">
+            {/* Tab 3.4 content */}
+          </Tab.Pane>
+        </Tab.Content>
+      </Col>
+    </Row>
+  </Tab.Container>
+);
+
+
+class MyNavbar extends Component {
+  constructor(state) {
+    super(state);
+    this.state = {
+      activeKey: 1
+    };
+  }
+
+  handleSelect(eventKey) {
+    this.setState({
+      activeKey: eventKey
+    });
+  }
+
+  render() {
+    return (
+      <Navbar inverse collapseOnSelect staticTop={true}>
+        <Row className="clearfix">
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">React-Bootstrap</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav bsStyle="pills" activeKey={this.state.activeKey}
+                 onSelect={this.handleSelect.bind(this)}>
+              <NavItem eventKey="1">Tab 1</NavItem>
+              <NavItem eventKey="2">Tab 2</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+          <Col sm={12}>
+            <Tab.Content animation>
+              <Tab.Pane eventKey="1">Tab 1 content
+              </Tab.Pane>
+              <Tab.Pane eventKey="2">Tab 2 content
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Navbar>
+    )
+  }
+}
+
+const mountNode = document.getElementById('container');
+
+/* ReactDOM.render(tabsInstance, mountNode);*/
+/* ReactDOM.render(<MyNavbar />, mountNode);*/
+ReactDOM.render(<Hello />, mountNode);
