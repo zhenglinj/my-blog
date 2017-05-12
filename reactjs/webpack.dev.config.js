bd7a18de8cb90bb0c4daa0d33b8bc4e62d022bae
zhenglinj <zhenglinj@hotmail.com>
2017-05-12 22:04:38 +0800

    Update nodejs express as react app backend

---
+++ reactjs/webpack.dev.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+const config = require('./webpack.config');
+
+const devConfig = Object.create(config);
+
+devConfig.entry = [
+  // Add the client which connects to our middleware
+  // You can use full urls like 'webpack-hot-middleware/client?path=http://localhost:3000/__webpack_hmr'
+  // useful if you run your app from another point like django
+  'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
+  // And then the actual application
+  './source/entry.jsx'
+];
+
+devConfig.output.publicPath = "/app/built";
+devConfig.output.filename = "bundle.js"
+
+devConfig.plugins = [
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NoErrorsPlugin(),
+  new webpack.DefinePlugin({
+    "__DEV__": true,
+    "process.env": {
+      NODE_ENV: JSON.stringify('development')
+    }
+  })
+];
+
+module.exports = devConfig;
