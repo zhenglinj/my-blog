7322db2c8f2f9d6b69fe8138e3ee85b01e13807b
zhenglinj <zhenglinj@hotmail.com>
2017-05-12 22:04:38 +0800

    Update nodejs express as react app backend

--- reactjs/source/entry.jsx
+++ reactjs/source/entry.jsx
@@ -2,5 +2,6 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import TmcTimer from './components/OnlineTimer.jsx';
 
-ReactDOM.render(<TmcTimer />,
-                document.getElementById('container'));
+ReactDOM.render(
+  <TmcTimer />,
+  document.getElementById('container'));
