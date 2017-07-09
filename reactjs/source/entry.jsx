3389bb92789a9c043327cc34c5e1f2410d5219f7
zhenglinj <zhenglinj@hotmail.com>
2017-07-09 22:21:57 +0800

    Update new theme

--- reactjs/source/entry.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Hello from './components/Hello.jsx';
-
-ReactDOM.render(
-  <Hello />,
-  document.getElementById('container'));
