b1efbcd16ed2b93e235d37c30a19d0c4dacdcd70
Linjun Zheng <zhenglinj@hotmail.com>
2018-08-04 16:56:01 +0800

    Fix search toggle and toc

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -6,7 +6,7 @@
  * Copyright 2017-2018 Michael Rose - mademistakes | @mmistakes
  * Free for personal and commercial use under the MIT license
  * https://github.com/mmistakes/jekyll-basically-theme/blob/master/LICENSE.md
-*/
+ */
 
 // Animate sidebar menu items
 var menuItems = document.querySelectorAll('#sidebar li');
@@ -17,30 +17,30 @@ var transitionProp = typeof docElemStyle.transition == 'string' ?
     'transition' : 'WebkitTransition';
 
 function animateMenuItems() {
-  for ( var i=0; i < menuItems.length; i++ ) {
-    var item = menuItems[i];
-    // Stagger transition with transitionDelay
-    item.style[ transitionProp + 'Delay' ] = ( i * 75 ) + 'ms';
-    item.classList.toggle('is--moved');
-  }
+    for ( var i=0; i < menuItems.length; i++ ) {
+        var item = menuItems[i];
+        // Stagger transition with transitionDelay
+        item.style[ transitionProp + 'Delay' ] = ( i * 75 ) + 'ms';
+        item.classList.toggle('is--moved');
+    }
 };
 
 // Toggle sidebar visibility
 function toggleClassMenu() {
-  myMenu.classList.add('is--animatable');
-  if(!myMenu.classList.contains('is--visible')) {
-    myMenu.classList.add('is--visible');
-    myToggle.classList.add('open');
-    myWrapper.classList.add('is--pushed');
-  } else {
-    myMenu.classList.remove('is--visible');
-    myToggle.classList.remove('open');
-    myWrapper.classList.remove('is--pushed');
-  }
+    myMenu.classList.add('is--animatable');
+    if(!myMenu.classList.contains('is--visible')) {
+        myMenu.classList.add('is--visible');
+        myToggle.classList.add('open');
+        myWrapper.classList.add('is--pushed');
+    } else {
+        myMenu.classList.remove('is--visible');
+        myToggle.classList.remove('open');
+        myWrapper.classList.remove('is--pushed');
+    }
 }
 
 function OnTransitionEnd() {
-  myMenu.classList.remove('is--animatable');
+    myMenu.classList.remove('is--animatable');
 }
 
 var myWrapper = document.querySelector('.wrapper');
@@ -48,20 +48,21 @@ var myMenu = document.querySelector('.sidebar');
 var myToggle = document.querySelector('.toggle');
 myMenu.addEventListener('transitionend', OnTransitionEnd, false);
 myToggle.addEventListener('click', function() {
-  toggleClassMenu();
-  animateMenuItems();
+    toggleClassMenu();
+    animateMenuItems();
 }, false);
 myMenu.addEventListener('click', function() {
-  toggleClassMenu();
-  animateMenuItems();
+    toggleClassMenu();
+    animateMenuItems();
 }, false);
 
 // Search toggle
 $(".search-toggle").on("click", function() {
-  $(".search-content").toggleClass("is--visible");
-  $(".initial-content").toggleClass("is--hidden");
-  // set focus on input
-  setTimeout(function() {
-    $("#search").focus();
-  }, 400);
+    $(".search-content").toggleClass("is--visible");
+    $(".initial-content").toggleClass("is--hidden");
+    $("#toc").toggleClass("is--hidden");
+    // set focus on input
+    setTimeout(function() {
+        $("#search").focus();
+    }, 400);
 });
